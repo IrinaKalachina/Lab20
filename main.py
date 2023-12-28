c80ad448da1b9ca1458a10105f50cc8b1f2f3db2
Irina <ira.kalachina@yandex.ru>
2023-12-28 11:04:45 +0700

    без ООП

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-def add_two_numbers(a, b):
-    return a + b
-
-def multiply_two_numbers(a, b):
-    return a * b
-
 def calculator(operation, a, b):
     if operation == 'addition':
-        return add_two_numbers(a, b)
+        return a + b
     elif operation == 'multiplication':
-        return multiply_two_numbers(a, b)
+        return a * b
     else:
         return None
 
